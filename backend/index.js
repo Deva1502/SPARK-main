054591b3ca0553b2667d7cfc14bbcf324e729873
Devansh Mishra <devanshm929@gmail.com>
2025-09-09 16:10:53 +0530

    hi

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ dotenv.config()
 
 const port=process.env.PORT || 5000
 app.use(cors({
-    origin:"*",
+    origin:"https://spark-main-tawny.vercel.app",
     credentials:true
 }))
 app.use(express.json())
