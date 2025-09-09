079bca2120cc0a0d7453dd39de260e5eabf7aa30
Devansh Mishra <devanshm929@gmail.com>
2025-09-09 16:11:17 +0530

    hihi

--- backend/socket.js
+++ backend/socket.js
@@ -6,7 +6,7 @@ const server=http.createServer(app)
 
 const io=new Server(server,{
     cors:{
-        origin:"*",
+        origin:"https://spark-main-tawny.vercel.app",
         methods:["GET","POST"]
     }
 })
